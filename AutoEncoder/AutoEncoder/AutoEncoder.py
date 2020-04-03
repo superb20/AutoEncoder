6c762629e866f7c1da9c3b45fa5d9fecdd6260dd
superb20 <superb20.lee@gmail.com>
2020-04-04 05:21:35 +0900

    Update latest codes

--- AutoEncoder/AutoEncoder/AutoEncoder.py
+++ AutoEncoder/AutoEncoder/AutoEncoder.py
@@ -5,19 +5,21 @@ from keras.models import Model
 from keras.datasets import mnist
 
 import numpy as np
+#from keras.preprocessing import image
+import cv2
 import matplotlib.pyplot as plt
 
 # size of encoded representation
-encoding_dim = 32
+encoding_dim = 600
 
 # Input place holder
-input_img = Input(shape=(784,))
+input_img = Input(shape=(60 * 36 * 3,))
 
 # input -> encoded
 encoded = Dense(encoding_dim, activation='relu')(input_img)
 
 # encoded -> decoded
-decoded = Dense(784, activation='sigmoid')(encoded)
+decoded = Dense(60 * 36* 3, activation='sigmoid')(encoded)
 
 # input -> encoded -> decoded
 autoencoder = Model(input_img, decoded)
@@ -35,7 +37,30 @@ decoder = Model(encoded_input, decoder_layer(encoded_input))
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 
-(x_train, _), (x_test, _) = mnist.load_data()
+x_train1 = []
+x_test1  = []
+
+for i in range(1, 14):
+    dataset_path = "data_set/"
+    name = "normal_" + str(i).zfill(4) + "_re.jpg"
+    print(name)
+    #img = image.load_img(dataset_path + name)
+    img = cv2.imread(dataset_path + name, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (60, 36))
+    result_path = "result_set/"
+    cv2.imwrite(result_path + name, img)
+
+    img = img.reshape(1, 60 * 36 * 3)
+    x_train1.append(img)
+    x_test1.append(img)
+
+    #x_train = np.concatenate(x_train, img)
+    #x_test = np.concatenate(x_train, img)
+    
+
+x_train = np.array(x_train1)
+x_test = np.array(x_test1)
+#(x_train, _), (x_test, _) = mnist.load_data()
 
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
@@ -45,7 +70,7 @@ print (x_train.shape)
 print (x_test.shape)
 
 autoencoder.fit(x_train, x_train,
-                epochs=50,
+                epochs=1000,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test, x_test))
@@ -60,15 +85,16 @@ plt.figure(figsize=(20, 4))
 for i in range(n):
     # origin data
     ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
+    RGB_img = cv2.cvtColor(x_test[i].reshape(36, 60, 3), cv2.COLOR_BGR2RGB)
+    plt.imshow(RGB_img)
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # recounstructed output
     ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
+    RGB_img = cv2.cvtColor(decoded_imgs[i].reshape(36, 60, 3), cv2.COLOR_BGR2RGB)
+    plt.imshow(RGB_img)
+
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
